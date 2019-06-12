5a51fddad8e5b4046d58953adc4c2d5dddb09425
Ole Rohne <o.m.rohne@fys.uio.no>
2019-06-12 19:03:45 +0200

    ZynqMP 2019-04 endpoint
    %% original patch: 0002-ZynqMP-2019-04-endpoint.patch

--- include/configs/enclustra_zynqmp.h
+++ include/configs/enclustra_zynqmp.h
@@ -12,6 +12,43 @@
 
 #include <configs/xilinx_zynqmp.h>
 
+#ifdef BOOTENV_DEV_DHCP
+#undef BOOTENV_DEV_DHCP
+#endif
+#define BOOTENV_DEV_DHCP(devtypeu, devtypel, instance)	\
+	"bootcmd_dhcp="					\
+	"if dhcp ${uenvaddr} ${bootfile}; then "	\
+	"env import -t ${uenvaddr} ${filesize}; "	\
+	"fi; "						\
+	"run $modeboot; "                               \
+	"\0"
+
+#ifdef BOOTENV_DEV_MMC
+#undef BOOTENV_DEV_MMC
+#endif
+#define BOOTENV_DEV_MMC(devtypeu, devtypel, instance)	 \
+	"bootcmd_mmc" #instance "="			 \
+	"setenv devnum " #instance "; "			 \
+	"mmc rescan; mmc dev ${devnum}; "		 \
+	"mmc info; mmc part; "				 \
+	"if load mmc ${devnum} ${uenvaddr} ${bootfile}; then " \
+	"env import -t {uenvaddr} ${filesize}; "	\
+	"fi; "				\
+        "run $modeboot; "		\
+	"\0"
+
+#ifdef BOOTENV_DEV_QSPI
+#undef BOOTENV_DEV_QSPI
+#endif
+#define BOOTENV_DEV_QSPI(devtypeu, devtypel, instance) \
+	"bootcmd_" #devtypel #instance "="             \
+	"sf probe " #instance " 0 0; " \
+	"if sf read $uenvaddr $script_offset_f $script_size_f; then "	\
+	"env import -t ${uenvaddr} $script_size_f; " \
+	"fi; "				\
+        "run $modeboot; "		\
+	"\0"
+
 #undef BOOT_TARGET_DEVICES
 #define BOOT_TARGET_DEVICES(func) \
         func(DHCP, dhcp, na) \
@@ -23,7 +60,26 @@
 /* Xilinx initial environment variables */
 #undef CONFIG_EXTRA_ENV_BOARD_SETTINGS
 #define CONFIG_EXTRA_ENV_BOARD_SETTINGS \
-	"ethaddr=10:65:30:0f:30:8f\0" \
+	"ethaddr=10:65:30:0f:30:8f\0"		\
+	"serverip=172.16.0.1\0"			\
+	"bootpath=ZynqMP/1065300f308f\0"	\
+	"fpgafile=download.bit\0"		\
+	"treefile=devicetree.dtb\0"		\
+	"rootfile=ramdisk.cpio.gz\0"		\
+	"bootfile=Image\0"			\
+	"uenvaddr=0x80000\0"			\
+	"fpgaaddr=0x100000\0"			\
+	"treeaddr=0x4000000\0"			\
+	"rootaddr=0x4010000\0"			\
+	"bootaddr=0x80000\0"			\
+	"netboot="					       \
+	"tftpboot $fpgaaddr $serverip:$bootpath/$fpgafile && " \
+	"fpga loadb 0 $fpgaaddr $filesize; "		       \
+	"tftpboot $bootaddr $serverip:$bootpath/$bootfile; " \
+	"tftpboot $rootaddr $serverip:$bootpath/$rootfile; " \
+	"tftpboot $treeaddr $serverip:$bootpath/$treefile; " \
+	"echo bootm $bootaddr $rootaddr $treeaddr\0" \
+	"modeboot=netboot\0" \
 	PARTS_DEFAULT
 
 #endif /* __CONFIG_ENCLUSTRA_ZYNQMP_H */
