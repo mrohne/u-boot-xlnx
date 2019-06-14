4dfee803bb32ac4d12878520b52a98d05947ae38
Ole Rohne <o.m.rohne@fys.uio.no>
2019-06-14 15:08:13 +0200

    Fixes to enclustra_zynqmp.h
    %% original patch: 0005-Fixes-to-enclustra_zynqmp.h.patch

--- include/configs/enclustra_zynqmp.h
+++ include/configs/enclustra_zynqmp.h
@@ -12,74 +12,89 @@
 
 #include <configs/xilinx_zynqmp.h>
 
-#ifdef BOOTENV_DEV_DHCP
-#undef BOOTENV_DEV_DHCP
+/* Allow to overwrite serial and ethaddr */
+#ifdef  CONFIG_ENV_OVERWRITE
+#undef  CONFIG_ENV_OVERWRITE
 #endif
+#define CONFIG_ENV_OVERWRITE
+
+/* enable preboot to be loaded before CONFIG_BOOTDELAY */
+#ifdef  CONFIG_PREBOOT
+#undef  CONFIG_PREBOOT
+#endif
+#define CONFIG_PREBOOT "run bootcmd_mmc1"
+
+#undef BOOTENV_DEV_DHCP
 #define BOOTENV_DEV_DHCP(devtypeu, devtypel, instance)	\
 	"bootcmd_dhcp="					\
-	"if dhcp ${uenvaddr} ${bootfile}; then "	\
+	"if dhcp ${uenvaddr}; then "	\
 	"env import -t ${uenvaddr} ${filesize}; "	\
 	"fi; "						\
-	"run $modeboot; "                               \
+	"env exists modeboot && run $modeboot; "                               \
 	"\0"
 
-#ifdef BOOTENV_DEV_MMC
 #undef BOOTENV_DEV_MMC
-#endif
-#define BOOTENV_DEV_MMC(devtypeu, devtypel, instance)	 \
-	"bootcmd_mmc" #instance "="			 \
-	"setenv devnum " #instance "; "			 \
-	"mmc rescan; mmc dev ${devnum}; "		 \
-	"mmc info; mmc part; "				 \
-	"if load mmc ${devnum} ${uenvaddr} ${bootfile}; then " \
-	"env import -t {uenvaddr} ${filesize}; "	\
-	"fi; "				\
-        "run $modeboot; "		\
+#define BOOTENV_DEV_MMC(devtypeu, devtypel, instance)	\
+	"bootcmd_mmc" #instance "="			\
+	"setenv devnum " #instance "; "			\
+	"mmc rescan; mmc dev ${devnum}; "		\
+	"mmc info; mmc part; "				\
+	"if load mmc ${devnum} ${uenvaddr} ${uenvfile}; then "	\
+	"env import -t {uenvaddr} ${filesize}; "		\
+	"fi; "							\
+        "env exists modeboot && run $modeboot; "		\
 	"\0"
 
-#ifdef BOOTENV_DEV_QSPI
 #undef BOOTENV_DEV_QSPI
-#endif
 #define BOOTENV_DEV_QSPI(devtypeu, devtypel, instance) \
 	"bootcmd_" #devtypel #instance "="             \
 	"sf probe " #instance " 0 0; " \
 	"if sf read $uenvaddr $script_offset_f $script_size_f; then "	\
 	"env import -t ${uenvaddr} $script_size_f; " \
 	"fi; "				\
-        "run $modeboot; "		\
+        "env exists modeboot && run $modeboot; "		\
 	"\0"
 
 #undef BOOT_TARGET_DEVICES
-#define BOOT_TARGET_DEVICES(func) \
-        func(DHCP, dhcp, na) \
-        func(MMC, mmc, 1) \
-	func(QSPI, qspi, 0) \
+#define BOOT_TARGET_DEVICES(func)		\
+  func(DHCP, dhcp, na)				\
+  func(MMC, mmc, 1)				\
+  func(QSPI, qspi, 0)				\
 
+#undef BOOTENV
+#define BOOTENV								\
+  BOOTENV_SHARED_MMC							\
+  BOOTENV_BOOT_TARGETS							\
+  BOOT_TARGET_DEVICES(BOOTENV_DEV)					\
+    "distro_bootcmd="							\
+    "for target in ${boot_targets}; do "				\
+    "run bootcmd_${target}; "						\
+    "done\0"								\
+
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND "run distro_bootcmd"
+
+#undef  ENV_MEM_LAYOUT_SETTINGS
+#define ENV_MEM_LAYOUT_SETTINGS \
+	"uenvsize=0x00080000\0"			\
+	"uenvaddr=0x02000000\0"			\
+	"rootaddr=0x02100000\0"			\
+	"treeaddr=0x04000000\0"			\
+	"bootaddr=0x18000000\0"			\
+	"fpgaaddr=0x40000000\0"			\
 
 /* Undef unneeded configs */
 /* Xilinx initial environment variables */
-#undef CONFIG_EXTRA_ENV_BOARD_SETTINGS
+#undef  CONFIG_EXTRA_ENV_BOARD_SETTINGS
 #define CONFIG_EXTRA_ENV_BOARD_SETTINGS \
 	"ethaddr=10:65:30:0f:30:8f\0"		\
-	"serverip=172.16.0.1\0"			\
-	"bootpath=ZynqMP/1065300f308f\0"	\
-	"fpgafile=download.bit\0"		\
-	"treefile=devicetree.dtb\0"		\
-	"rootfile=ramdisk.cpio.gz\0"		\
-	"bootfile=Image\0"			\
-	"uenvaddr=0x80000\0"			\
-	"fpgaaddr=0x100000\0"			\
-	"treeaddr=0x4000000\0"			\
-	"rootaddr=0x4010000\0"			\
-	"bootaddr=0x80000\0"			\
-	"netboot="					       \
-	"tftpboot $fpgaaddr $serverip:$bootpath/$fpgafile && " \
-	"fpga loadb 0 $fpgaaddr $filesize; "		       \
-	"tftpboot $bootaddr $serverip:$bootpath/$bootfile; " \
-	"tftpboot $rootaddr $serverip:$bootpath/$rootfile; " \
-	"tftpboot $treeaddr $serverip:$bootpath/$treefile; " \
-	"echo bootm $bootaddr $rootaddr $treeaddr\0" \
-	"modeboot=netboot\0" \
-	PARTS_DEFAULT
+	"uenvfile=uEnv.txt\0"                   \
+	"testaddr=0xdeadbeef\0"			\
+
+#undef  CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS	\
+	CONFIG_EXTRA_ENV_BOARD_SETTINGS \
+	ENV_MEM_LAYOUT_SETTINGS		\
+	BOOTENV				\
 
 #endif /* __CONFIG_ENCLUSTRA_ZYNQMP_H */
